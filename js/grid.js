509d2b84c3e476471da91216f123a6d8daf3baf5
Jirka Chadima <chadima@jtbank.cz>
2012-11-05 16:33:11 +0100

    disabled buttons don't bother with alerts, added bootstrap stuff

--- js/grid.js
+++ js/grid.js
@@ -1,158 +1,171 @@
 $(function(){
-    $.ajaxSetup({
-        success: function(data){
-            if(data.redirect){
-                $.get(data.redirect);
-            }
-            if(data.snippets){
-                for (var snippet in data.snippets){
-                    $("#"+snippet).html(data.snippets[snippet]);
-                }
-            }
-        }
-    });
+	$.ajaxSetup({
+		success: function(data){
+			if(data.redirect){
+				$.get(data.redirect);
+			}
+			if(data.snippets){
+				for (var snippet in data.snippets){
+					$("#"+snippet).html(data.snippets[snippet]);
+				}
+			}
+		}
+	});
 
-    $(".grid-flash-hide").live("click", function(){
-        $(this).parent().parent().fadeOut(300);
-    });
+	$(".grid-flash-hide").live("click", function() {
+		$(this).parent().parent().fadeOut(300);
+	});
 
-    $(".grid-select-all").live("click", function(){
-        var checkboxes =  $(this).parents("thead").siblings("tbody").children("tr:not(.grid-subgrid-row)").find("td input:checkbox.grid-action-checkbox");
-        if($(this).is(":checked")){
-            $(checkboxes).attr("checked", "checked");
-        }else{
-            $(checkboxes).removeAttr("checked");
-        }
-    });
+	$(".grid-select-all").live("click", function(){
+		var checkboxes =  $(this).parents("thead").siblings("tbody").children("tr:not(.grid-subgrid-row)").find("td input:checkbox.grid-action-checkbox");
+		if($(this).is(":checked")){
+			$(checkboxes).attr("checked", "checked");
+		}else{
+			$(checkboxes).removeAttr("checked");
+		}
+	});
 
-    $('.grid a.grid-ajax:not(.grid-confirm)').live('click', function (event) {
-        event.preventDefault();
-        $.get(this.href);
-    });
+	$('.grid a.grid-ajax:not(.grid-confirm)').live('click', function (event) {
+		event.preventDefault();
+		$.get(this.href);
+	});
 
-    $('.grid a.grid-confirm:not(.grid-ajax)').live('click', function (event) {
-        var answer = confirm($(this).data("grid-confirm"));
-        return answer;
-    });
+	$('.grid a.grid-confirm:not(.grid-ajax)').live('click', function (event) {
+		var $this = $(this);
+		if ($this.hasClass('click-disabled')) {
+			return null;
+		}
+		var answer = confirm($(this).data("grid-confirm"));
+		return answer;
+	});
 
-    $('.grid a.grid-confirm.grid-ajax').live('click', function (event) {
-        event.preventDefault();
-        var answer = confirm($(this).data("grid-confirm"));
-        if(answer){
-            $.get(this.href);
-        }
-    });
+	$('.grid a.grid-confirm.grid-ajax').live('click', function (event) {
+		event.preventDefault();
+		var $this = $(this);
+		if ($this.hasClass('click-disabled')) {
+			return;
+		}
+		var answer = confirm($this.data("grid-confirm"));
+		if(answer){
+			$.get(this.href);
+		}
+	});
 
-    $(".grid-gridForm").find("input[type=submit]").live("click", function(){
-        $(this).addClass("grid-gridForm-clickedSubmit");
-    });
+	$(".grid-gridForm").find("input[type=submit]").live("click", function(){
+		$(this).addClass("grid-gridForm-clickedSubmit");
+	});
 
 
-    $(".grid-gridForm").live("submit", function(event){
-        var button = $(".grid-gridForm-clickedSubmit");
-        $(button).removeClass("grid-gridForm-clickedSubmit");
-        if($(button).data("select")){
-            var selectName = $(button).data("select");
-            var option = $("select[name=\""+selectName+"\"] option:selected");
-            if($(option).data("grid-confirm")){
-                var answer = confirm($(option).data("grid-confirm"));
-                if(answer){
-                    if($(option).hasClass("grid-ajax")){
-                        event.preventDefault();
-                        $.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
-                    }
-                }else{
-                    return false;
-                }
-            }else{
-                if($(option).hasClass("grid-ajax")){
-                    event.preventDefault();
-                    $.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
-                }
-            }
-        }else{
-            event.preventDefault();
-            $.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
-        }
-    });
+	$(".grid-gridForm").live("submit", function(event){
+		var button = $(".grid-gridForm-clickedSubmit");
+		$(button).removeClass("grid-gridForm-clickedSubmit");
+		if($(button).data("select")){
+			var selectName = $(button).data("select");
+			var option = $("select[name=\""+selectName+"\"] option:selected");
+			if($(option).data("grid-confirm")){
+				var answer = confirm($(option).data("grid-confirm"));
+				if(answer){
+					if($(option).hasClass("grid-ajax")){
+						event.preventDefault();
+						$.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
+					}
+				}else{
+					return false;
+				}
+			}else{
+				if($(option).hasClass("grid-ajax")){
+					event.preventDefault();
+					$.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
+				}
+			}
+		}else{
+			event.preventDefault();
+			$.post(this.action, $(this).serialize()+"&"+$(button).attr("name")+"="+$(button).val());
+		}
+	});
 
-    $(".grid-autocomplete").live('keydown.autocomplete', function(){
-        var gridName = $(this).data("gridname");
-        var column = $(this).data("column");
-        var link = $(this).data("link");
-        $(this).autocomplete({
-            source: function(request, response) {
-                $.ajax({
-                    url: link,
-                    data: gridName+'-term='+request.term+'&'+gridName+'-column='+column,
-                    dataType: "json",
-                    method: "post",
-                    success: function(data) {
-                        response(data.payload);
-                    }
-                });
-            },
-            delay: 100,
-            open: function() { $('.ui-menu').width($(this).width()) }
-        });
-    });
+	$(".grid-autocomplete").live('keydown.autocomplete', function(){
+		var gridName = $(this).data("gridname");
+		var column = $(this).data("column");
+		var link = $(this).data("link");
+		$(this).autocomplete({
+			source: function(request, response) {
+				$.ajax({
+					url: link,
+					data: gridName+'-term='+request.term+'&'+gridName+'-column='+column,
+					dataType: "json",
+					method: "post",
+					success: function(data) {
+						response(data.payload);
+					}
+				});
+			},
+			delay: 100,
+			open: function() {
+				$('.ui-menu').width($(this).width())
+				}
+		});
+	});
 
-    $(".grid-changeperpage").live("change", function(){
-        $.get($(this).data("link"), $(this).data("gridname")+"-perPage="+$(this).val());
-    });
+	$(".grid-changeperpage").live("change", function(){
+		$.get($(this).data("link"), $(this).data("gridname")+"-perPage="+$(this).val());
+	});
 
-    function hidePerPageSubmit()
-    {
-        $(".grid-perpagesubmit").hide();
-    }
-    hidePerPageSubmit();
+	function hidePerPageSubmit()
+	{
+		$(".grid-perpagesubmit").hide();
+	}
+	hidePerPageSubmit();
 
-    function setDatepicker()
-    {
-        $.datepicker.regional['cs'] = {
-            closeText: 'Zav≈ô√≠t',
-            prevText: '&#x3c;D≈ô√≠ve',
-            nextText: 'Pozdƒõji&#x3e;',
-            currentText: 'Nyn√≠',
-            monthNames: ['leden','√∫nor','b≈ôezen','duben','kvƒõten','ƒçerven',
-                'ƒçervenec','srpen','z√°≈ô√≠','≈ô√≠jen','listopad','prosinec'],
-            monthNamesShort: ['led','√∫no','b≈ôe','dub','kvƒõ','ƒçer',
-                'ƒçvc','srp','z√°≈ô','≈ô√≠j','lis','pro'],
-            dayNames: ['nedƒõle', 'pondƒõl√≠', '√∫ter√Ω', 'st≈ôeda', 'ƒçtvrtek', 'p√°tek', 'sobota'],
-            dayNamesShort: ['ne', 'po', '√∫t', 'st', 'ƒçt', 'p√°', 'so'],
-            dayNamesMin: ['ne','po','√∫t','st','ƒçt','p√°','so'],
-            weekHeader: 'T√Ωd',
-            dateFormat: 'yy-mm-dd',
-            constrainInput: false,
-            firstDay: 1,
-            isRTL: false,
-            showMonthAfterYear: false,
-            yearSuffix: ''};
-        $.datepicker.setDefaults($.datepicker.regional['cs']);
+	function setDatepicker()
+	{
+		$.datepicker.regional['cs'] = {
+			closeText: 'Zav¯Ìt',
+			prevText: '&#x3c;D¯Ìve',
+			nextText: 'PozdÏji&#x3e;',
+			currentText: 'NynÌ',
+			monthNames: ['leden','˙nor','b¯ezen','duben','kvÏten','Ëerven',
+			'Ëervenec','srpen','z·¯Ì','¯Ìjen','listopad','prosinec'],
+			monthNamesShort: ['led','˙no','b¯e','dub','kvÏ','Ëer',
+			'Ëvc','srp','z·¯','¯Ìj','lis','pro'],
+			dayNames: ['nedÏle', 'pondÏlÌ', '˙ter˝', 'st¯eda', 'Ëtvrtek', 'p·tek', 'sobota'],
+			dayNamesShort: ['ne', 'po', '˙t', 'st', 'Ët', 'p·', 'so'],
+			dayNamesMin: ['ne','po','˙t','st','Ët','p·','so'],
+			weekHeader: 'T˝d',
+			dateFormat: 'yy-mm-dd',
+			constrainInput: false,
+			firstDay: 1,
+			isRTL: false,
+			showMonthAfterYear: false,
+			yearSuffix: ''
+		};
+		$.datepicker.setDefaults($.datepicker.regional['cs']);
 
-        $(".grid-datepicker").each(function(){
-            if(($(this).val() != "")){
-                var date = $.datepicker.formatDate('yy-mm-dd', new Date($(this).val()));
-            }
-            $(this).datepicker();
-            $(this).datepicker({ constrainInput: false});
-        });
-    }
-    setDatepicker();
+		$(".grid-datepicker").each(function(){
+			if(($(this).val() != "")){
+				var date = $.datepicker.formatDate('yy-mm-dd', new Date($(this).val()));
+			}
+			$(this).datepicker();
+			$(this).datepicker({
+				constrainInput: false
+			});
+		});
+	}
+	setDatepicker();
 
-    $(this).ajaxStop(function(){
-        setDatepicker();
-        hidePerPageSubmit();
-    });
+	$(this).ajaxStop(function(){
+		setDatepicker();
+		hidePerPageSubmit();
+	});
 
-    $("input.grid-editable").live("keypress", function(e) {
-        if (e.keyCode == '13') {
-            e.preventDefault();
-            $("input[type=submit].grid-editable").click();
-        }
-    });
+	$("input.grid-editable").live("keypress", function(e) {
+		if (e.keyCode == '13') {
+			e.preventDefault();
+			$("input[type=submit].grid-editable").click();
+		}
+	});
 
-    $("table.grid tbody tr:not(.grid-subgrid-row) td.grid-data-cell").live("dblclick", function(e) {
-        $(this).parent().find("a.grid-editable:first").click();
-    });
+	$("table.grid tbody tr:not(.grid-subgrid-row) td.grid-data-cell").live("dblclick", function(e) {
+		$(this).parent().find("a.grid-editable:first").click();
+	});
 });
