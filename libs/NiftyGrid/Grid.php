8302a611781a091e51344f1da84f2d25b5a1bcd8
Jirka Chadima <chadima@jtbank.cz>
2014-10-22 15:57:06 +0200

    should fix #3, untested

--- libs/NiftyGrid/Grid.php
+++ libs/NiftyGrid/Grid.php
@@ -853,6 +853,9 @@ abstract class Grid extends \Nette\Application\UI\Control {
 		$paginators = array();
 		foreach ($values as $gridName => $grid) {
 			$isSubGrid = ($gridName == $this->name) ? FALSE : TRUE;
+			if (!array_key_exists('filter', $grid)) {
+				continue;
+			}
 			foreach ($grid['filter'] as $name => $value) {
 				if ($value != '') {
 					if ($name == "send") {
